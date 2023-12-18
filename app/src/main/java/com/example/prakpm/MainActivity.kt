d53f228d32eef81874d2929af0b547e5928114bc
raihan <raihanjamillah0404@gmail.com>
2023-12-18 21:00:45 +0700

    MainActivity dan UpdateAddActivity

--- app/src/main/java/com/example/prakpm/MainActivity.kt
+++ app/src/main/java/com/example/prakpm/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.prakpm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.btnTambah
+import kotlinx.android.synthetic.main.activity_main.rvCategory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CrudView {
+    private lateinit var presenter: Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter = Presenter(this)
+        presenter.getData()
+
+        btnTambah.setOnClickListener{
+            startActivity(Intent(applicationContext, UpdateAddActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onSuccessGet(data: List<DataItem>?) {
+        rvCategory.adapter = DataAdapter(data, object : DataAdapter.onClickItem {
+            override fun clicked(item:DataItem?) {
+                val bundle = Bundle()
+                bundle.putSerializable("dataItem", item)
+                val intent = Intent(applicationContext, UpdateAddActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+            override fun delete(item: DataItem?){
+                presenter.hapusData(item?.staffId)
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+            }
+        })
+    }
+
+    override fun onFailedGet(msg: String) {
+    }
+
+    override fun onSuccessDelete(msg: String) {
+        presenter.getData()
+    }
+
+    override fun onErrorDelete(msg: String) {
+        Toast.makeText(this, "Delete tidak berhasil", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSuccessAdd(msg: String) {
+    }
+
+    override fun errorAdd(msg: String) {
+    }
+
+    override fun onSuccessUpdate(msg: String) {
+    }
+
+    override fun onErrorupdate(msg: String) {
     }
 }
